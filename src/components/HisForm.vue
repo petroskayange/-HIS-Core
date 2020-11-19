f8af23c06970ad4f71a1b4c2d61594493e646b1a
Jacob Mziya <jacobmziya@gmail.com>
2020-11-19 12:24:09 +0200

    Created an HIS form component.

---
+++ src/components/HisForm.vue
@@ -0,0 +1,43 @@
+<template>
+  <form novalidate>
+    <ion-list class="get-centered">
+      <ion-item>
+        <ion-input
+            name="first_name"
+            type="text"
+            spellcheck="false"
+            autocapitalize="off"
+            required
+            placeholder="First Name"
+        ></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-input
+            name="last_name"
+            type="text"
+            spellcheck="false"
+            autocapitalize="off"
+            required
+            placeholder="Last Name"
+        ></ion-input>
+      </ion-item>
+    </ion-list>
+  </form>
+</template>
+
+<script>
+import {IonInput, IonItem, IonList} from "@ionic/vue";
+
+export default {
+  name: "HisForm",
+  components: {
+    IonInput,
+    IonList,
+    IonItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
