0e877d7021b34635489af2c25b0a409afd4c33d9
Andrew Mfune <andiemfune@gmail.com>
2021-05-19 16:03:43 +0200

    Added Single select element

---
+++ src/components/FormElement/HisSelect.vue
@@ -0,0 +1,28 @@
+<template>
+    <div> 
+        <ion-list>
+            <ion-item v-for="(item, index) in items" :key="index" @click="onselect(Item)"> 
+                <ion-label> {{item.label}} </ion-label>
+            </ion-item>
+        </ion-list>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import {  Option } from '../Form/Field'
+
+export default defineComponent({
+    name: "HisSelect",
+    props: {
+        items: {
+            required: true,
+            type: Object as PropType<Option[]> 
+        }
+    },
+    methods: {
+        onselect(item: Option): void {
+            this.$emit('onValue', item)
+        }
+    }
+})
+</script>
