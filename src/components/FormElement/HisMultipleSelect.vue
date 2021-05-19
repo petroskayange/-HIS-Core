a039979a8b36105f243ab038012b951bb9cf3809
Andrew Mfune <andiemfune@gmail.com>
2021-05-19 16:36:30 +0200

    Added Element for selecting multiple items

---
+++ src/components/FormElement/HisMultipleSelect.vue
@@ -0,0 +1,46 @@
+<template>
+    <div> 
+        <ion-list>
+            <ion-item v-for="(data, index) in listData" :key="index"> 
+                <ion-label> 
+                    <ion-grid> 
+                        <ion-row>
+                            <ion-col> <ion-checkbox :modelValue="data.isChecked"/> </ion-col>
+                            <ion-col> <ion-label> {{data.label}} </ion-label> </ion-col>
+                        </ion-row>
+                    </ion-grid>
+            </ion-item>
+        </ion-list>
+    </div>
+</template>
+<script lang="ts">
+import { Option } from "../Form/Field";
+import { defineComponent, PropType } from "vue";
+import { IonCheckbox, IonItem, IonLabel, IonList} from "@ionic/vue"
+export default defineComponent({
+  components: { IonCheckbox, IonItem, IonLabel, IonList},
+  name: "HisMultipleSelect",
+  props: {
+    items: {
+      required: true,
+      type: Object as PropType<Option[]>,
+    },
+  },
+  watch: {
+    listData: {
+        handler(updatedItems: Array<Option>){
+            this.$emit('onValue', updatedItems.map(item => item.isChecked))
+        },
+        deep: true
+    }
+  },
+  data() {
+    return {
+      listData: [] as Array<Option>,
+    };
+  },
+  mounted(){
+    this.listData = [...this.items]
+  }
+});
+</script>
