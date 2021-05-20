819ab80542287041605e97c488250f10accad9f7
Martin Suleman <sulemartin87@gmail.com>
2021-05-20 16:12:18 +0200

    worked on a scaffold for routing to reports

--- src/components/ART/reports.vue
+++ src/components/ART/reports.vue
@@ -1,13 +1,96 @@
 <template>
   <div>
-      reports
+    <div v-show="activeReports.length == 0">
+      <ion-button
+        v-for="(report, index) in Object.keys(reports)"
+        :key="index"
+        @click="showReports(report)"
+        size="large"
+        color="medium"
+      >
+        {{ report }}
+      </ion-button>
+    </div>
+    <div v-if="activeReports.length > 0">
+      <ion-button @click="activeReports = []" color="danger">back</ion-button>
+      <br />
+      <ion-button
+        v-for="(innerreport, idx) in activeReports"
+        :key="idx"
+        size="default"
+        color="medium"
+      >
+        {{ innerreport.name }}
+      </ion-button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+import { defineComponent } from "vue";
+import { IonButton } from "@ionic/vue";
+export default defineComponent({
+  components: {
+    IonButton,
+  },
+  data() {
+    return {
+      reports: {
+        clinic: [
+          {
+            name: "Cohort / disaggregated",
+            route: "/",
+          },
+          {
+            name: "Survival analysis",
+            route: "/",
+          },
+          {
+            name: "TPT new initiations",
+            route: "/",
+          },
+        ],
+        MoH: [
+          {
+            name: "Cohort / disaggregated",
+            route: "/",
+          },
+          {
+            name: "Survival analysis",
+            route: "/",
+          },
+          {
+            name: "TPT new initiations",
+            route: "/",
+          },
+        ],
+        PEPFAR: [
+          {
+            name: "Cohort / disaggregated",
+            route: "/",
+          },
+          {
+            name: "Survival analysis",
+            route: "/",
+          },
+          {
+            name: "TPT new initiations",
+            route: "/",
+          },
+        ],
+      },
+      activeReports: [],
+    };
+  },
+  methods: {
+    showReports(report) {
+      this.activeReports = [...this.reports[report]];
+    },
+  },
+  mounted() {
+    //
+  },
+});
 </script>
 
 <style>
