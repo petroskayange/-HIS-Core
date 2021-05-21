60106e699d3b2d3be2c4208523b32b593f5b0663
Andrew Mfune <andiemfune@gmail.com>
2021-05-21 13:01:23 +0200

    Added additional demo fields

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -4,6 +4,7 @@
       :fields="fields"
       :next="isNext"
       :prev="isPrev"
+      @onErrors="onErrors"
       @onNext="updateNext"
       @onPrev="updatePrev"
       @onFinish="onFinish"
@@ -24,65 +25,10 @@ import { IonPage } from "@ionic/vue";
 import { FieldType } from "@/components/Forms/FieldType";
 export default defineComponent({
   components: { WizardForm, IonPage, HisFooter },
-  data() {
-    return {
-      isNext: false,
-      isPrev: false,
-      fields: [
-        {
-          id: "1234",
-          helpText: "Select Multiple values",
-          type: FieldType.TT_MULTIPLE_SELECT,
-          options: [
-            {
-              label: "Foo",
-              value: "Foo",
-            },
-            {
-              label: "Baz",
-              value: "Baz",
-            },
-            {
-              label: "Bar",
-              value: "Bar",
-            },
-          ],
-        },
-        {
-          id: "1234",
-          helpText: "Click on value to proceed",
-          type: FieldType.TT_SELECT,
-          requireNext: false,
-          options: [
-            {
-              label: "Duke",
-              value: "Duke",
-            },
-            {
-              label: "Dork",
-              value: "Dork",
-            },
-          ],
-        },
-        {
-          id: "1234",
-          helpText: "Select one value and click next",
-          type: FieldType.TT_SELECT,
-          options: [
-            {
-              label: "Fur",
-              value: "Fur",
-            },
-            {
-              label: "Fume",
-              value: "Fame",
-            },
-          ],
-        }
-      ],
-    };
-  },
   methods: {
+    onErrors(errors){
+      alert(errors.join(', '))
+    },
     onCancel(){
       this.$router.push({path: "/"})
     },
@@ -101,5 +47,109 @@ export default defineComponent({
       console.log('Form was submitted')
     }
   },
+  data() {
+    return {
+      isNext: false,
+      isPrev: false,
+      fields: [
+        {
+          id: "multiple_select",
+          helpText: "Select Multiple values",
+          type: FieldType.TT_MULTIPLE_SELECT,
+          options: [
+            {
+              label: "Foo",
+              value: "Foo",
+            },
+            {
+              label: "Baz",
+              value: "Baz",
+            },
+            {
+              label: "Bar",
+              value: "Bar",
+            },
+          ],
+        },
+        {
+          id: 'conditional_data',
+          helpText: "Conditionally display next question",
+          type: FieldType.TT_SELECT,
+          options: [
+            {
+              label: "Show Games",
+              value: "Show Games"
+            },
+            {
+              label: "Show Music",
+              value: "Show Music"
+            }
+          ]
+        },
+        {
+          id: 'music_presentation',
+          helpText: 'Showing Music',
+          type: FieldType.TT_SELECT,
+          condition(formData) {
+            return formData.conditional_data.value === 'Show Music' 
+          },
+          options: [
+            {
+              label: 'No Music',
+              value: 'No Music'
+            }
+          ]
+        },
+        {
+          id: 'games_presentation',
+          helpText: 'Showing Games',
+          type: FieldType.TT_SELECT,
+          condition(formData) {
+            return formData.conditional_data.value === 'Show Games'
+          },
+          options: [
+            {
+              label: 'No Games',
+              value: 'No Games'
+            }
+          ]
+        },
+        {
+          id: "does_not_require_next",
+          helpText: "Click on value to proceed",
+          type: FieldType.TT_SELECT,
+          requireNext: false,
+          options: [
+            {
+              label: "Duke",
+              value: "Duke",
+            },
+            {
+              label: "Dork",
+              value: "Dork",
+            },
+          ],
+        },
+        {
+          id: "validated",
+          helpText: "Value is Required before next",
+          type: FieldType.TT_SELECT,
+          validation(value) {
+            return !value ? ['Value is required']: null
+          },
+          options: [
+            {
+              label: "Fur",
+              value: "Fur",
+            },
+            {
+              label: "Fume",
+              value: "Fame",
+            },
+          ],
+        }
+      ],
+    };
+  }
 });
 </script>
