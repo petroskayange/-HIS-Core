467c08e96f4ce1ec2ba5c9367175cee5bd34abff
Andrew Mfune <andiemfune@gmail.com>
2021-05-23 10:49:09 +0200

    Added custom input field

---
+++ src/components/FormElements/HisTextInput.vue
@@ -0,0 +1,42 @@
+<template> 
+    <ion-input
+      :value="value"
+      type="text"
+      class="input_display"
+      :disabled="disabled"
+    />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import {IonInput} from '@ionic/vue'
+export default defineComponent({
+    name: "HisInput",
+    components: { IonInput},
+    props: {
+        value: {
+            required: false
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        disabled:{
+            type: Boolean
+        }
+    }
+})
+</script>
+<style scoped>
+.input_display {
+  border: 1px solid rgb(94, 91, 91);
+  border-radius: 5px;
+  width: 100%;
+  font-family: Nimbus Sans L, Arial Narrow, sans-serif;
+  font-size: 2.2em;
+  background-color: rgb(231, 231, 231);
+  color: #000;
+  padding: 5px;
+  margin-bottom: 5px;
+  border-radius: 5px;
+}
+</style>
