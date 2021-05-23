136ddc45bb9984f0422e3aa05f41bf089edd83c3
Andrew Mfune <andiemfune@gmail.com>
2021-05-23 11:36:17 +0200

    Using standard form component for testing

--- src/views/Example.vue
+++ src/views/Example.vue
@@ -1,77 +1,29 @@
 <template>
-  <ion-page>
-    <base-form
-      :fields="fields"
-      :next="isNext"
-      :prev="isPrev"
-      :clear="isClear"
-      @onClear="isClear=false"
-      @onErrors="onErrors"
-      @onNext="updateNext"
-      @onPrev="updatePrev"
-      @onFinish="onFinish"
-    />
-    <his-footer
-      :showBack="showBack"
-      :showNext="showNext"
-      :showFinish="showFinish"
-      :showClear="true"
-      @onCancel="onCancel"
-      @onClear="isClear=true"
-      @onNext="isNext=true"
-      @onBack="isPrev=true"
-      @onFinish="onSubmit"
-    ></his-footer>
-  </ion-page>
-</template>
-<script>
-import BaseForm from "@/components/Forms/BaseForm.vue";
-import HisFooter from "@/components/HisNavFooter.vue";
+  <his-standard-form :fields="fields" @onSubmit="onSubmit" @onFinish="onFinish"/>
+</template> 
+<script lang="ts">
 import { defineComponent } from "vue";
-import { IonPage } from "@ionic/vue";
-import { FieldType } from "@/components/Forms/FieldType";
+import { FieldType } from "@/components/Forms/FieldType"
+import HisStandardForm from "@/components/Forms/HisStandardForm.vue";
 export default defineComponent({
-  components: { BaseForm, IonPage, HisFooter },
+  components: { HisStandardForm },
   methods: {
-    onErrors(errors){
-      alert(errors.join(', '))
-    },
-    onCancel(){
-      this.$router.push({path: "/"})
-    },
-    onFinish() {
-      this.showFinish = true
-      this.showNext = false
-    },
-    updateNext({ index }) {
-      this.isNext = false
-      if (index >= 1) this.showBack = true
-    },
-    updatePrev({ index }) {
-      this.isPrev = false
-      this.showNext = true
-      this.showFinish = false
-      if (index === 0) this.showBack = false
+    onFinish(formData: any) {
+      console.log(formData)
     },
     onSubmit() {
-      console.log('Form was submitted')
-    }
+      console.log("Form has been submitted");
+    },
   },
   data() {
     return {
-      showNext: true,
-      showBack: false,
-      showFinish: false,
-      isClear: false,
-      isNext: false,
-      isPrev: false,
       fields: [
         {
           id: "multiple_select",
           helpText: "Select Multiple values",
           type: FieldType.TT_MULTIPLE_SELECT,
-          validation(value) {
-            return !value ? ['Value is required']: null
+          validation(value: any) {
+            return !value ? ["Value is required"] : null;
           },
           options: [
             {
@@ -89,65 +41,65 @@ export default defineComponent({
           ],
         },
         {
-          id: 'conditional_data',
+          id: "conditional_data",
           helpText: "Conditionally display next question",
           type: FieldType.TT_SELECT,
-          requireNext:false,
-          validation(value) {
-            return !value ? ['Value is required']: null
+          requireNext: false,
+          validation(value: any) {
+            return !value ? ["Value is required"] : null;
           },
           options: [
             {
               label: "Show Games",
-              value: "Show Games"
+              value: "Show Games",
             },
             {
               label: "Show Music",
-              value: "Show Music"
-            }
-          ]
+              value: "Show Music",
+            },
+          ],
         },
         {
-          id: 'music_presentation',
-          helpText: 'Showing Music',
+          id: "music_presentation",
+          helpText: "Showing Music",
           type: FieldType.TT_SELECT,
-          validation(value) {
-            return !value ? ['Value is required']: null
+          validation(value: any) {
+            return !value ? ["Value is required"] : null;
           },
-          condition(formData) {
-            return formData.conditional_data.value === 'Show Music' 
+          condition(formData: any) {
+            return formData.conditional_data.value === "Show Music";
           },
           options: [
             {
-              label: 'No Music',
-              value: 'No Music'
-            }
-          ]
+              label: "No Music",
+              value: "No Music",
+            },
+          ],
         },
         {
-          id: 'games_presentation',
-          helpText: 'Showing Games',
+          id: "games_presentation",
+          helpText: "Showing Games",
           type: FieldType.TT_SELECT,
-          validation(value) {
-            return !value ? ['Value is required']: null
+          validation(value: any) {
+            return !value ? ["Value is required"] : null;
           },
-          condition(formData) {
-            return formData.conditional_data.value === 'Show Games'
+          condition(formData: any) {
+            return formData.conditional_data.value === "Show Games";
           },
           options: [
             {
-              label: 'No Games',
-              value: 'No Games'
-            }
-          ]
+              label: "No Games",
+              value: "No Games",
+            },
+          ],
         },
         {
           id: "does_not_require_next",
           helpText: "Click on value to proceed",
           type: FieldType.TT_SELECT,
           requireNext: false,
-          validation(value) {
-            return !value ? ['Value is required']: null
+          validation(value: any) {
+            return !value ? ["Value is required"] : null;
           },
           options: [
             {
@@ -164,8 +116,8 @@ export default defineComponent({
           id: "validated",
           helpText: "Value is Required before next",
           type: FieldType.TT_SELECT,
-          validation(value) {
-            return !value ? ['Value is required']: null
+          validation(value: any) {
+            return !value ? ["Value is required"] : null;
           },
           options: [
             {
@@ -177,9 +129,9 @@ export default defineComponent({
               value: "Fame",
             },
           ],
-        }
+        },
       ],
     };
-  }
+  },
 });
 </script>
