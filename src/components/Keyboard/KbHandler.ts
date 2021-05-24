8244a1071d38b839144448647b2178b2fad4fa06
Andrew Mfune <andiemfune@gmail.com>
2021-05-24 10:50:09 +0200

    Added checks to handle spaces and deletes

--- src/components/Keyboard/KbHandler.ts
+++ src/components/Keyboard/KbHandler.ts
@@ -1,6 +1,13 @@
-export default function kbHandler(
-    newInput: string | number, 
-    accumulator: string | number): string {
+export default function kbHandler(newInput: string, accumulator: string): string {
+    let output = accumulator
 
-    return `${accumulator}${newInput}`
+    if (newInput.match(/delete/i)) {
+        output = output.substring(0, output.length - 1)
+    } else if (newInput.match(/space/i)) {
+        output = `${accumulator} `
+    } else {
+        output = `${accumulator}${newInput}`
+    }
+    return output
+
 }
