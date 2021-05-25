83e6b13533832ae0da2b48b1b3a7ee9a9e112c27
Andrew Mfune <andiemfune@gmail.com>
2021-05-25 15:08:41 +0200

    Fixed and updated keyboard layout

--- src/components/Keyboard/KbLayouts.ts
+++ src/components/Keyboard/KbLayouts.ts
@@ -2,26 +2,26 @@ export const NUMBER_ONLY = [
     ['1', '2', '3'],
     ['4', '5', '6'],
     ['7', '8', '9'],
-    [''   ,0,  '' ]
+    ['0']
 ]
 
 export const ALPHABETICAL = [
-    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', "'"],
-    ['i', 'j', 'k', 'l', 'm', 'n', 'p', 'p', 'qwerty'],
+    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', '.'],
+    ['i', 'j', 'k', 'l', 'm', 'n', 'p', 'p', "qwerty"],
     ['q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 ]
 
 export const QWERTY = [
-    ['q', 'w', 'e', 'r', 'y', 'u', 'i', 'o', 'p'],
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'i', "'"],
-    ['z', 'x', 'c', 'v', 'b', 'm', 'n', ',', '.', 'A-Z']
+    ['z', 'x', 'c', 'v', 'b', 'm', 'n', ',', '.']
 ]
 
 export const CHARACTERS_AND_NUMBERS = [
     ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
     ['q', 'w', 'e', 'r', 'y', 'u', 'i', 'o', 'p'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'i', "'"],
-    ['z', 'x', 'c', 'v', 'b', 'm', ',', '.', 'A-Z'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'i'],
+    ['z', 'x', 'c', 'v', 'b', 'm', ',', '.', "'"],
 ]
 
 export const ALPHABETICAL_FUNCTION_KEYS = [
@@ -31,12 +31,13 @@ export const ALPHABETICAL_FUNCTION_KEYS = [
 ]
 
 export const QWERTY_FUNCTION_KEYS = [
-    ['Delete'],
-    ['0-9'],
-    ['Space']
+    ['0-9', 'A-Z'],
+    ['Space', 'Delete'],
+    ['Unknown']
 ]
 
 export const NUM_FUNCTION_KEYS = [
     ['A-Z', 'qwerty'],
-    ['Unknown', 'Delete']
+    ['Space', 'Delete'],
+    ['Unknown']
 ]
