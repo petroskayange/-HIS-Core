4674c729c53c91bd49f76a5c916beabad582c2a7
Andrew Mfune <andiemfune@gmail.com>
2021-06-04 11:13:28 +0200

    Unknown button now overrides entire input value

--- src/components/Keyboard/KbHandler.ts
+++ src/components/Keyboard/KbHandler.ts
@@ -5,6 +5,8 @@ export default function kbHandler(newInput: string, accumulator: string): string
         output = output.substring(0, output.length - 1)
     } else if (newInput.match(/space/i)) {
         output = `${accumulator} `
+    } else if (newInput.match(/unknown/i)) {
+        output = 'Unknown'
     } else {
         output = `${accumulator}${newInput}`
     }
