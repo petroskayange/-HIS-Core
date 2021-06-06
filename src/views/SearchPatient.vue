f34597a0f035ef263ab0698888a67fb676993e59
Andrew Mfune <andiemfune@gmail.com>
2021-06-06 09:43:40 +0200

    Added basic validations

--- src/views/SearchPatient.vue
+++ src/views/SearchPatient.vue
@@ -7,6 +7,7 @@ import { defineComponent, PropType } from "vue";
 import { FieldType } from "@/components/Forms/BaseFormElements"
 import { Field } from "@/components/Forms/FieldInterface"
 import HisStandardForm from "@/components/Forms/HisStandardForm.vue";
+import { isName, required } from "@/components/Forms/validations/StandardValidations"
 
 export default defineComponent({
   components: { HisStandardForm },
@@ -27,18 +28,21 @@ export default defineComponent({
         {
           id: 'given_name',
           helpText: 'First name',
-          type: FieldType.TT_TEXT
+          type: FieldType.TT_TEXT,
+          validation: (value: any) => isName(value)
         },
         {
           id: 'family_name',
           helpText: "Last name",
-          type: FieldType.TT_TEXT
+          type: FieldType.TT_TEXT,
+          validation: (value: any) => isName(value)
         },
         {
           id: 'gender',
           helpText: 'Gender',
           type: FieldType.TT_SELECT,
           requireNext: false,
+          validation: (value: any) => required(value),
           options: () => ([
             { 
                 label: 'Male',
