f502489244edbe849800de1602bb9c45b9dc709f
Andrew Mfune <andiemfune@gmail.com>
2021-06-07 14:39:39 +0200

    refactored personattribute interface

--- src/services/PersonAttributes.ts
+++ src/services/PersonAttributes.ts
@@ -1,11 +1,11 @@
 import ApiClient from './api_client'
 
-export interface PersonAttributes {
+export interface PersonAttribute {
     person_attribute_type_id: number,
     value: number | string
 }
 
-export async function createAttributes(data: Array<PersonAttributes>) {
+export async function createAttributes(data: Array<PersonAttribute>) {
     const req = await ApiClient.post('/person_attributes', data)
     
     if (req && req.ok) return req?.json()
