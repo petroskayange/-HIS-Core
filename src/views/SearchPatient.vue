8974637f3e477d33c5751acfcbdd021f2b075558
Andrew Mfune <andiemfune@gmail.com>
2021-06-07 09:56:21 +0200

    Added dynamic search for patient names

--- src/views/SearchPatient.vue
+++ src/views/SearchPatient.vue
@@ -6,8 +6,10 @@
 import { defineComponent } from "vue";
 import { FieldType } from "@/components/Forms/BaseFormElements"
 import { Field } from "@/components/Forms/FieldInterface"
+import { Option } from "@/components/Forms/FieldInterface"
 import HisStandardForm from "@/components/Forms/HisStandardForm.vue";
 import StdValidaton from "@/components/Forms/validations/StandardValidations"
+import Person from "@/services/Person"
 
 export default defineComponent({
   components: { HisStandardForm },
@@ -24,24 +26,42 @@ export default defineComponent({
     onSubmit() {
       console.log("Form has been submitted");
     },
-    getFields: (): Array<Field> => ([
+    mapToOption(listOptions: Array<string>): Array<Option> {
+        return listOptions.map((item: any) => ({
+            label: item,
+            value: item
+        })) 
+    },
+    getFields: function(): Array<Field> {
+      return [
         {
           id: 'given_name',
           helpText: 'First name',
           type: FieldType.TT_TEXT,
-          validation: (value: any) => StdValidaton.isName(value)
+          validation: (value: any) => StdValidaton.isName(value),
+          options: async (form: any) => {
+            if (!form.given_name || form.given_name.value === null) return []
+
+            const names = await Person.searchGivenName(form.given_name.value)
+            return this.mapToOption(names)
+          }
         },
         {
           id: 'family_name',
           helpText: "Last name",
           type: FieldType.TT_TEXT,
-          validation: (value: any) => StdValidaton.isName(value)
+          validation: (value: any) => StdValidaton.isName(value),
+          options: async (form: any) => {
+            if (!form.family_name || form.family_name.value === null) return []
+
+            const names = await Person.searchFamilyName(form.family_name.value)
+            return this.mapToOption(names)
+          }
         },
         {
           id: 'gender',
           helpText: 'Gender',
           type: FieldType.TT_SELECT,
-          requireNext: false,
           validation: (value: any) => StdValidaton.required(value),
           options: () => ([
             { 
@@ -54,7 +74,8 @@ export default defineComponent({
             }
           ])
         },
-    ])
+      ]
+    }
   }
 })
 </script>
