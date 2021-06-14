87113e486b1ce146837bc0a5a9c33335895ab032
Andrew Mfune <andiemfune@gmail.com>
2021-06-14 10:49:50 +0200

    Added result card component

---
+++ src/components/DataViews/HisResultCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <ion-card>
+        <ion-card-header>
+            <ion-card-title> {{title}} </ion-card-title>
+            <ion-card-subtitle>
+                <ion-icon :icon="icon" size="medium"></ion-icon> 
+                {{ subtitle }} 
+            </ion-card-subtitle>
+        </ion-card-header>
+    </ion-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { IonCard, IonIcon, IonCardTitle, IonCardSubtitle, IonCardHeader } from "@ionic/vue"
+export default defineComponent({
+    name: "HisResultCard",
+    components: {IonCard, IonIcon, IonCardTitle, IonCardSubtitle, IonCardHeader},
+    props: {
+        title: {
+            required: true,
+            type: String
+        },
+        subtitle: {
+            required: true,
+            type: String
+        },
+        icon: {
+            required: false
+        }
+    }
+})
+</script>
