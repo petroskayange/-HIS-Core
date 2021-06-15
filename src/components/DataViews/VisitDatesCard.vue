15d389b9a6cf8cb20736cc04b5e47c7fb35516ca
Andrew Mfune <andiemfune@gmail.com>
2021-06-15 16:07:43 +0200

    Can now switch to different dates

--- src/components/DataViews/VisitDatesCard.vue
+++ src/components/DataViews/VisitDatesCard.vue
@@ -1,38 +1,73 @@
 <template>
-    <div class="card"> 
-        <h4> <center> {{title}} </center> </h4>
-        <div v-for="(item, index) in items" :key="index"> 
-            <center><ion-button color="light" class="btn"> {{item.label}} </ion-button></center>
-        </div>
+  <div class="card">
+    <h4>
+      <center>{{ title }}</center>
+    </h4>
+    <div v-for="(item, index) in items" :key="index">
+      <center>
+        <ion-button
+          :color="isActive(item) ? 'primary' : 'light'"
+          class="btn"
+          @click="onselect(item)"
+        >
+          {{ item.label }}
+        </ion-button>
+      </center>
     </div>
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
-import { Option } from "@/components/Forms/FieldInterface"
-import { IonButton } from "@ionic/vue"
+import { defineComponent, PropType } from "vue";
+import { Option } from "@/components/Forms/FieldInterface";
+import { IonButton } from "@ionic/vue";
 
 export default defineComponent({
-    components: { IonButton },
-    props: {
-        title: {
-            type: String,
-            default: () => 'Visits'
-        },
-        items: {
-            type: Object as PropType<Option[]>,
-            required: true
+  components: { IonButton },
+  props: {
+    title: {
+      type: String,
+      default: () => "Visits",
+    },
+    items: {
+      type: Object as PropType<Option[]>,
+      required: true,
+    },
+  },
+  data: () => ({
+    active: {} as Option,
+  }),
+  watch: {
+    items: {
+      handler(items: Array<Option> | undefined) {
+        if (Array.isArray(items)){
+            this.active = items[0];
         }
-    }
-})
+      },
+      deep: true,
+      immediate: true,
+    },
+    active(item: Option) {
+      this.$emit("onselect", item);
+    },
+  },
+  methods: {
+    onselect(item: Option) {
+      this.active = item;
+    },
+    isActive(item: Option) {
+      return item.label === this.active.label;
+    },
+  },
+});
 </script>
 <style scoped>
 .btn {
-    width: 90%;
-    text-align: center;
-    font-weight: bold;
-    margin-bottom: 20px;
+  width: 90%;
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 20px;
 }
-.card{
+.card {
   border-radius: 6px;
   border: 1px solid #ccc;
   height: 70vh;
