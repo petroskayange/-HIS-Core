72e4d3589916e9ee13b726ba1edc3fd7684eea4c
Martin Suleman <sulemartin87@gmail.com>
2021-06-15 09:09:36 +0200

    fixed order service error

--- src/services/order_service.ts
+++ src/services/order_service.ts
@@ -11,22 +11,22 @@ export class OrderService extends Service {
     }
 
     static getViralLoadOrders(orders: Order[]) {
-       return orders.filter(order => {
-        //    try {
-               
-        //    } catch (error) {
-               
-        //    }
-           const result = order.tests[0].result;
-           if(!Array.isArray(result) ) return false
-            return order.tests.length > 0 && order.tests[0].result.length > 0;
-       });
+        return orders.filter(order => {
+            try {
+                const result = order.tests[0].result;
+                if (!Array.isArray(result)) return false
+                return order.tests.length > 0 && order.tests[0].result.length > 0;
+            } catch (error) {
+                console.log(error);
+            }
+
+        });
     }
     static formatOrders(order: Order) {
-         const test = order.tests[0];
-         const result = test.result[0];
-         const resultDate = HisDate.toStandardHisFormat(result.date);
-         return `${test.name} ${result.value_modifier}${result.value} ${resultDate}`;
+        const test = order.tests[0];
+        const result = test.result[0];
+        const resultDate = HisDate.toStandardHisFormat(result.date);
+        return `${test.name} ${result.value_modifier}${result.value} ${resultDate}`;
     }
-    
+
 }
