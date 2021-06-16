bb51216083aeab32f5b816a7afcb609e70488d92
Andrew Mfune <andiemfune@gmail.com>
2021-06-16 16:46:05 +0200

    Added wrapper methods for session storage

--- src/services/service.ts
+++ src/services/service.ts
@@ -2,6 +2,7 @@ import ApiClient from "./api_client"
 import Url from "@/utils/Url"
 
 export class Service {
+    // TODO: remove these
     static programID = sessionStorage.programID;
     static userRoles = sessionStorage.userRoles;
     static sessionDate = sessionStorage.sessionDate;
@@ -14,17 +15,62 @@ export class Service {
 
         if (req && req.ok) return req?.json()
     }
-    static getSessionDate() {
-        return sessionStorage.sessionDate;
-    }
-    static getProgramID() {
-        return sessionStorage.programID;
-    }
+    
     static async postJson(url: string, data: any, genericError='Unable to save record') {
         const req = await ApiClient.post(url, data)
     
         if (req && req.ok) return req?.json()
 
         throw genericError
-    }   
+    }
+
+    static getUserLocation() {
+        return sessionStorage.getItem('userLocation')
+    }
+
+    static getSessionDate() {
+        return sessionStorage.getItem('sessionDate');
+    }
+    
+    static getProgramID() {
+        return sessionStorage.getItem('programID');
+    }
+    
+    static getApplicationImage() {
+        return sessionStorage.getItem('applicationImage')
+    }
+
+    static getApplicationName() {
+        return sessionStorage.getItem('applicationName')
+    }
+    
+    static getUserRoles() {
+       const roles = sessionStorage.getItem('userRoles');
+       
+       if (roles) return JSON.parse(roles)
+    }
+
+    static setUserLocation(name: string) {
+        sessionStorage.setItem('userLocation', name)
+    }
+    
+    static setSessionDate(date: string) {
+        sessionStorage.setItem('sessionDate', date);
+    }
+    
+    static setProgramID(id: string) {
+        sessionStorage.setItem('programID', id);
+    }
+    
+    static setApplicationImage(url: string) {
+        sessionStorage.setItem('applicationImage', url)
+    }
+
+    static setApplicationName(name: string) {
+        sessionStorage.setItem('applicationName', name)
+    }
+    
+    static setUserRoles(roles: any) {
+        sessionStorage.setItem('userRoles', JSON.stringify(roles))
+    }
 }
