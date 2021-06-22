e96951ff01410a576c5306cbac30995613507d7c
Andrew Mfune <andiemfune@gmail.com>
2021-06-22 08:55:48 +0200

    Initial Pagination of visit dates

--- src/components/DataViews/VisitDatesCard.vue
+++ src/components/DataViews/VisitDatesCard.vue
@@ -3,23 +3,30 @@
     <h4>
       <center>{{ title }}</center>
     </h4>
-    <div v-for="(item, index) in items" :key="index">
-      <center>
+    <center>
+    <ion-button color="light" v-if="showPrevButton" @click="goPrev">
+      <img src="/assets/images/drop-up-arrow.svg" width="30"/>
+    </ion-button>
+    <div v-for="(item, index) in activeListItems" :key="index">
         <ion-button
-          :color="isActive(item) ? 'primary' : 'light'"
           class="btn"
+          :color="isActive(item) ? 'primary' : 'light'"
           @click="onselect(item)"
         >
           {{ item.label }}
         </ion-button>
-      </center>
     </div>
+    <ion-button color="light" v-if="showNextButton" @click="goNext">
+      <img src="/assets/images/drop-down-arrow.svg" width="30"/>
+    </ion-button>
+    </center>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
 import { Option } from "@/components/Forms/FieldInterface";
 import { IonButton } from "@ionic/vue";
+import { chunk, isEmpty } from "lodash"
 
 export default defineComponent({
   components: { IonButton },
@@ -35,17 +42,38 @@ export default defineComponent({
   },
   data: () => ({
     active: {} as Option,
+    activeListItems: [] as any,
+    paginatedListItems: [] as any,
+    index: -1,
+    perPage: 7
   }),
+  computed: {
+    showNextButton(): boolean {
+      return this.index +1 < this.paginatedListItems.length
+    },
+    showPrevButton(): boolean {
+      return this.index >= 1
+    }
+  },
   watch: {
     items: {
       handler(items: Array<Option> | undefined) {
-        if (Array.isArray(items)){
-            this.active = items[0];
-        }
+        if (isEmpty(items)) return
+
+        this.index = 0
+        this.paginatedListItems = chunk(items, this.perPage)
+        this.activeListItems = this.paginatedListItems[0]
+        this.active = this.activeListItems[0]
       },
       deep: true,
       immediate: true,
     },
+    index : {
+      handler(num: number){
+        this.activeListItems = this.paginatedListItems[num]
+      },
+      immediate: true
+    },   
     active(item: Option) {
       this.$emit("onselect", item);
     },
@@ -57,6 +85,17 @@ export default defineComponent({
     isActive(item: Option) {
       return item.label === this.active.label;
     },
+    goNext() {
+      const nextIndex = this.index + 1
+      if (nextIndex <= this.paginatedListItems.length) {
+        this.index = nextIndex
+      }
+    },
+    goPrev() {
+      if (this.paginatedListItems.length >= 1) {
+        this.index -= 1
+      }
+    }
   },
 });
 </script>
@@ -67,12 +106,12 @@ export default defineComponent({
   margin-bottom: 20px;
 }
 .card {
-  overflow-y: auto;
+  overflow: hidden;
   height: 74vh;
   background-color: rgb(255, 255, 255);
   border-right: 1px solid #ccc;
--webkit-box-shadow: 10px 0px 17px -12px rgba(0,0,0,0.4);
--moz-box-shadow: 10px 0px 17px -12px rgba(0,0,0,0.4);
-box-shadow: 10px 0px 17px -12px rgba(0,0,0,0.4);
+  -webkit-box-shadow: 10px 0px 17px -12px rgba(0,0,0,0.4);
+  -moz-box-shadow: 10px 0px 17px -12px rgba(0,0,0,0.4);
+  box-shadow: 10px 0px 17px -12px rgba(0,0,0,0.4);
 }
 </style>
