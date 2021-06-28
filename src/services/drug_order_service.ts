4d6dc043a84fc7ee32cfd4378bb0f96c5f2cd165
Andrew Mfune <andiemfune@gmail.com>
2021-06-28 10:51:02 +0200

    New Endpoint for creating drug orders

--- src/services/drug_order_service.ts
+++ src/services/drug_order_service.ts
@@ -1,15 +1,20 @@
 import { Service } from "./service";
-
+import { DrugOrder } from "@/interfaces/DrugOrder";
 export class DrugOrderService extends Service {
     constructor() {
         super()
     }
 
-    public static getOrderByPatient(patientId: number, params: any) {
+    static create(orders: DrugOrder) {
+        return this.postJson('drug_orders', orders)
+    }
+
+    static getOrderByPatient(patientId: number, params: any) {
         return this.getJson('/drug_orders', {
             'patient_id': patientId,
             'program_id': super.getProgramID(),
             ...params
         })
     }
+
 }
