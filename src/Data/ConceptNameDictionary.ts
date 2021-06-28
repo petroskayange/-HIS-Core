1ee272c74eb456647dd0d276c3f41a4f24f6ed4c
Andrew Mfune <andiemfune@gmail.com>
2021-06-28 14:23:37 +0200

    Added concepts for regimen prescription

--- src/Data/ConceptNameDictionary.ts
+++ src/Data/ConceptNameDictionary.ts
@@ -1,4 +1,11 @@
 export default [
+    { 'name': 'Appointment type', 'concept_id': 6784 },
+    { 'name': 'Rifapentine', 'concept_id': 9974 },
+    { 'name': 'Assess for fast track', 'concept_id': 9561 },
+    { 'name': 'Reason for ARV switch', 'concept_id': 1779 },
+    { 'name': 'Fast track', 'concept_id': 8471 },
+    { 'name': 'Medication orders', 'concept_id': 1282 },
+    { 'name': 'Antiretroviral drugs', 'concept_id': 1085 },
     { 'name': 'Art Side effects', 'concept_id': 7755 },
     { 'name': 'Yes', 'concept_id': 1065 },
     { 'name': 'No', 'concept_id': 1066 },
