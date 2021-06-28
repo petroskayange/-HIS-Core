2661ec0de41c525ccd1ada7ed4b9093a1182e945
Andrew Mfune <andiemfune@gmail.com>
2021-06-28 10:37:06 +0200

    Added api for retrieving custom regimen and current patient regimen

--- src/services/regimen_service.ts
+++ src/services/regimen_service.ts
@@ -8,4 +8,12 @@ export class RegimenService extends Service {
     static getRegimens() {
         return this.getJson(`programs/${this.getProgramID()}/regimens`)
     }
+
+    static getCustomIngridients() {
+        return this.getJson(`programs/${this.getProgramID()}/custom_regimen_ingredients`)
+    }
+
+    static getCurrentRegimen(patientId: number, date=this.getSessionDate()) {
+        return this.getJson(`programs/${this.getProgramID()}/${patientId}`, {date})   
+    }
 }
