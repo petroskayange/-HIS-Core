b4dd12d3eed23f0f177928df79448dd8539dd911
Martin Suleman <sulemartin87@gmail.com>
2021-07-02 13:29:19 +0200

    worked on routing to settings components

---
+++ src/apps/ART/preferences.ts
@@ -0,0 +1,10 @@
+import SiteCode from "@/components/SetSiteCode.vue"
+
+// Reference names for BaseFormComponents
+export enum FieldType {
+    TT_SET_SITE_CODE="site-code"
+}
+// Components to be rendered
+export const BasePrefernceComponents = {
+    SiteCode: SiteCode
+}
