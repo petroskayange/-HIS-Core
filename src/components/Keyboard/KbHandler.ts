094e2edaa766075dac95125482a6703b85789379
Andrew Mfune <andiemfune@gmail.com>
2021-07-03 23:15:27 +0200

    Remove period from delete button

--- src/components/Keyboard/KbHandler.ts
+++ src/components/Keyboard/KbHandler.ts
@@ -1,7 +1,7 @@
 export default function kbHandler(newInput: string, accumulator: string): string {
     let output = accumulator
 
-    if (newInput.match(/delete|del./i)) {
+    if (newInput.match(/delete|del/i)) {
         output = output.substring(0, output.length - 1)
     } else if (newInput.match(/space/i)) {
         output = `${accumulator} `
