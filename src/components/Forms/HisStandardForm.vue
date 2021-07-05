2c51c7ef8ca32a8d7df51b69d675f08550c55a74
Andrew Mfune <andiemfune@gmail.com>
2021-07-05 16:45:29 +0200

    Summary page reincarnation is now a regular field

--- src/components/Forms/HisStandardForm.vue
+++ src/components/Forms/HisStandardForm.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-content>
         <base-form
-            :fields="fields"
+            :fields="formFields"
             :next="isNext"
             :prev="isPrev"
             :clear="isClear"
@@ -21,15 +21,21 @@
 <script lang="ts">
 import BaseForm from "@/components/Forms/BaseForm.vue";
 import HisFooter from "@/components/HisDynamicNavFooter.vue";
-import { Field } from "./FieldInterface";
+import { Field, Option } from "./FieldInterface";
+import { FieldType } from "@/components/Forms/BaseFormElements"
 import { defineComponent, PropType } from "vue";
 import { IonPage, IonContent } from "@ionic/vue";
 import { alertConfirmation, toastWarning } from "@/utils/Alerts"
 import { NavBtnInterface } from "@/components/HisDynamicNavFooterInterface"
+import { find } from "lodash"
 export default defineComponent({
     name: "HisStandardForm",
     components: { BaseForm, IonPage, IonContent, HisFooter },
     props: {
+        skipSummary: {
+            type: Boolean,
+            default: false
+        },
         fields: {
             type: Object as PropType<Field[]>,
             required: true
@@ -42,6 +48,7 @@ export default defineComponent({
     },
     data:()=>({
       index: 0,
+      formFields: [] as Array<Field>,
       totalFields: 0,
       onNextRequired: true,
       isClear: false,
@@ -50,7 +57,9 @@ export default defineComponent({
       footerBtns: [] as Array<NavBtnInterface>
     }),
     created() {
-        this.totalFields = this.fields.length
+        const summary = this.skipSummary ? [] : [this.getDefaultSummaryField()]
+        this.formFields = [...this.fields, ...summary]
+        this.totalFields = this.formFields.length
         this.footerBtns = [
             this.cancel(),
             this.clear(),
@@ -77,6 +86,35 @@ export default defineComponent({
         },
         onFinish(formData: Record<string, any>) {
             this.$emit('onFinish', formData)
+        },
+        getDefaultSummaryField(): Field {
+            return {
+                id: '__form_summary__',
+                helpText: 'Summary',
+                type: FieldType.TT_SUMMARY,
+                options: (formData: Record<string, any>) => {
+                    const data: Array<Option> = [];
+                    const resolveLabel = (ref: string) => {
+                        return find(this.fields, {id: ref})?.helpText || 'Field'
+                    }
+                    for(const ref in formData) {
+                        const fdata = formData[ref]
+
+                        if (!fdata) continue
+
+                        if (Array.isArray(fdata)) {
+                            fdata.forEach(item => {
+                                data.push({  label: resolveLabel(ref), value: item.label })
+                            })
+                            continue
+                        }
+
+                        data.push({ label: resolveLabel(ref), value: fdata.label})
+                    }
+                    console.log(data)
+                    return data
+                }
+            }
         },
         cancel(): NavBtnInterface {
             return {
