0eee8a5bb654670f0d28abd31df83dc08b59748c
Martin Suleman <sulemartin87@gmail.com>
2021-07-07 17:06:16 +0200

    added site selection setting

--- src/apps/ART/preferences.ts
+++ src/apps/ART/preferences.ts
@@ -3,14 +3,15 @@ import ClinicDays from "@/components/SetClinicDays.vue"
 import HtnAge from "@/components/SetHTNAge.vue"
 import AppointmentLimit from "@/components/SetAppointmentLimit.vue"
 import FilingNumbers from "@/components/SetFilingNumbersLimit.vue"
-
+import SiteLocation from "@/components/SetSiteLocation.vue"
 // Reference names for BaseFormComponents
 export enum FieldType {
     TT_SET_SITE_CODE="site-code",
     TT_SET_CLINC_DAY="clinic-days",
     TT_SET_HTN_AGE="htn-age",
     TT_SET_APPOINTMENT_LIMIT="appointment-limit",
-    TT_SET_FILING_NUMBERS_LIMIT="filing-numbers"  
+    TT_SET_FILING_NUMBERS_LIMIT="filing-numbers" ,
+    TT_SET_SITE_LOCATION="site-location" 
 }
 // Components to be rendered
 export const BasePrefernceComponents = {
@@ -18,5 +19,6 @@ export const BasePrefernceComponents = {
     ClinicDays: ClinicDays,
     HtnAge: HtnAge,
     AppointmentLimit: AppointmentLimit,
-    FilingNumbers: FilingNumbers
+    FilingNumbers: FilingNumbers,
+    SiteLocation: SiteLocation
 }
