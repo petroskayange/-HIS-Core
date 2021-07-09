28384c11f580d171192e2257478c69d886906232
Andrew Mfune <andiemfune@gmail.com>
2021-07-09 18:38:47 +0200

    WHO staging concepts

--- src/Data/ConceptNameDictionary.ts
+++ src/Data/ConceptNameDictionary.ts
@@ -17,4 +17,316 @@ export default [
     { 'name': 'Type of patient', 'concept_id':  3289},
     { 'name': 'Patient present', 'concept_id':  1805},
     { 'name': 'Guardian present', 'concept_id':  2122},
+    { 'name': 'External consultation', 'concept_id': 9684 }, 
+    { 'name': 'Type of patient', 'concept_id':  3289},
+    {
+        "name": "Cryptococcal meningitis or other extrapulmonary cryptococcosis",
+        "concept_id": 7548,
+        "category": "who_staging"
+    },
+    {
+        "name": "Candidiasis of oseophagus, trachea and bronchi or lungs",
+        "concept_id": 5340,
+        "category": "who_staging"
+    },
+    {
+        "name": "Extrapulmonary tuberculosis (EPTB)",
+        "concept_id": 1547,
+        "category": "who_staging"
+    },
+    {
+        "name": "Kaposis sarcoma",
+        "concept_id": 507,
+        "category": "who_staging"
+    },
+    {
+        "name": "Bacterial pneumonia, severe recurrent",
+        "concept_id": 1215,
+        "category": "who_staging"
+    },
+    {
+        "name": "Non-typhoidal Salmonella bacteraemia, recurrent",
+        "concept_id": 7959,
+        "category": "who_staging"
+    },
+    {
+        "name": "Symptomatic HIV-associated nephropathy or cardiomyopathy",
+        "concept_id": 7957,
+        "category": "who_staging"
+    },
+    {
+        "name": "Cerebral or B-cell non Hodgkin lymphoma",
+        "concept_id": 2587,
+        "category": "who_staging"
+    },
+    {
+        "name": "Pneumocystis pneumonia",
+        "concept_id": 882,
+        "category": "who_staging"
+    },
+    {
+        "name": "Chronic herpes simplex infection (orolabial, gential / anorectal >1 month or visceral at any site)",
+        "concept_id": 5344,
+        "category": "who_staging"
+    },
+    {
+        "name": "Cytomegalovirus infection (retinitis or infection or other organs)",
+        "concept_id": 7551,
+        "category": "who_staging"
+    },
+    {
+        "name": "Toxoplasmosis of the brain",
+        "concept_id": 2583,
+        "category": "who_staging"
+    },
+    {
+        "name": "Invasive cancer of cervix",
+        "concept_id": 2588,
+        "category": "who_staging"
+    },
+    {
+        "name": "Unspecified stage 4 condition",
+        "concept_id": 7040,
+        "category": "who_staging"
+    },
+    {
+        "name": "Other",
+        "concept_id": 6408,
+        "category": "who_staging"
+    },
+    {
+        "name": "HIV encephalopathy",
+        "concept_id": 1362,
+        "category": "who_staging"
+    },
+    {
+        "name": "Disseminated non-tuberculosis mycobacterial infection",
+        "concept_id": 2585,
+        "category": "who_staging"
+    },
+    {
+        "name": "Cryptosporidiosis, chronic with diarroea",
+        "concept_id": 7549,
+        "category": "who_staging"
+    },
+    {
+        "name": "Isosporiasis >1 month",
+        "concept_id": 7956,
+        "category": "who_staging"
+    },
+    {
+        "name": "Disseminated mycosis (coccidiomycosis or histoplasmosis)",
+        "concept_id": 7550,
+        "category": "who_staging"
+    },
+    {
+        "name": "Progressive multifocal leukoencephalopathy",
+        "concept_id": 5046,
+        "category": "who_staging"
+    },
+    {
+        "name": "Severe unexplained wasting or malnutrition not responding to treatment (weight-for-height/ -age <70% or MUAC less than 11cm or oedema)",
+        "concept_id": 3068,
+        "category": "who_staging"
+    },
+    {
+        "name": "Bacterial infections, severe recurrent  (empyema, pyomyositis, meningitis, bone/joint infections but EXCLUDING pneumonia)",
+        "concept_id": 2894,
+        "category": "who_staging"
+    },
+    {
+        "name": "Chronic herpes simplex infection (orolabial or cutaneous >1 month or visceral at any site)",
+        "concept_id": 7960,
+        "category": "who_staging"
+    },
+    {
+        "name": "Cytomegalovirus infection: rentinitis or other organ (from age 1 month)",
+        "concept_id": 7552,
+        "category": "who_staging"
+    },
+    {
+        "name": "Toxoplasmosis of the brain (from age 1 month)",
+        "concept_id": 5048,
+        "category": "who_staging"
+    },
+    {
+        "name": "Recto-vaginal fistula, HIV-associated",
+        "concept_id": 7961,
+        "category": "who_staging"
+    },
+    {
+        "name": "Severe weight loss >10% and/or BMI <18.5kg/m^2, unexplained",
+        "concept_id": 7540,
+        "category": "who_staging"
+    },
+    {
+        "name": "Diarrhoea, chronic (>1 month) unexplained",
+        "concept_id": 5018,
+        "category": "who_staging"
+    },
+    {
+        "name": "Fever, persistent unexplained, intermittent or constant, >1 month",
+        "concept_id": 5027,
+        "category": "who_staging"
+    },
+    {
+        "name": "Pulmonary tuberculosis (current)",
+        "concept_id": 8206,
+        "category": "who_staging"
+    },
+    {
+        "name": "Tuberculosis (PTB or EPTB) within the last 2 years",
+        "concept_id": 7539,
+        "category": "who_staging"
+    },
+    {
+        "name": "Oral candidiasis",
+        "concept_id": 5334,
+        "category": "who_staging"
+    },
+    {
+        "name": "Anaemia, unexplained < 8 g/dl",
+        "concept_id": 2582,
+        "category": "who_staging"
+    },
+    {
+        "name": "Neutropaenia, unexplained < 500 /mm(cubed)",
+        "concept_id": 7954,
+        "category": "who_staging"
+    },
+    {
+        "name": "Severe bacterial infections (pneumonia, empyema, pyomyositis, bone/joint, meningitis, bacteraemia)",
+        "concept_id": 7541,
+        "category": "who_staging"
+    },
+    {
+        "name": "Thrombocytopaenia, chronic < 50,000 /mm(cubed)",
+        "concept_id": 7955,
+        "category": "who_staging"
+    },
+    {
+        "name": "Hepatitis B or C infection",
+        "concept_id": 8205,
+        "category": "who_staging"
+    },
+    {
+        "name": "Oral hairy leukoplakia",
+        "concept_id": 5337,
+        "category": "who_staging"
+    },
+    {
+        "name": "Unspecified stage 3 condition",
+        "concept_id": 7039,
+        "category": "who_staging"
+    },
+    {
+        "name": "Moderate unexplained wasting/malnutrition not responding to treatment (weight-for-height/ -age 70-79% or muac 11-12 cm)",
+        "concept_id": 7543,
+        "category": "who_staging"
+    },
+    {
+        "name": "Diarrhoea, persistent unexplained (14 days or more)",
+        "concept_id": 7544,
+        "category": "who_staging"
+    },
+    {
+        "name": "Oral candidiasis (from age 2 months)",
+        "concept_id": 7545,
+        "category": "who_staging"
+    },
+    {
+        "name": "Acute necrotizing ulcerative gingivitis or periodontitis",
+        "concept_id": 7546,
+        "category": "who_staging"
+    },
+    {
+        "name": "Lymph node tuberculosis",
+        "concept_id": 7547,
+        "category": "who_staging"
+    },
+    {
+        "name": "Symptomatic lymphoid interstitial pneumonia",
+        "concept_id": 5024,
+        "category": "who_staging"
+    },
+    {
+        "name": "Chronic HIV-associated lung disease, including bronchiectasis",
+        "concept_id": 2889,
+        "category": "who_staging"
+    },
+    {
+        "name": "Moderate weight loss less than or equal to 10 percent, unexplained",
+        "concept_id": 5332,
+        "category": "who_staging"
+    },
+    {
+        "name": "Respiratory tract infections, recurrent (sinusitis, tonsilitus, otitis media, pharyngitis)",
+        "concept_id": 5012,
+        "category": "who_staging"
+    },
+    {
+        "name": "Seborrhoeic dermatitis",
+        "concept_id": 2578,
+        "category": "who_staging"
+    },
+    {
+        "name": "Papular pruritic eruptions / Fungal nail infections",
+        "concept_id": 7536,
+        "category": "who_staging"
+    },
+    {
+        "name": "Herpes zoster",
+        "concept_id": 836,
+        "category": "who_staging"
+    },
+    {
+        "name": "Angular cheilitis",
+        "concept_id": 2575,
+        "category": "who_staging"
+    },
+    {
+        "name": "Oral ulcerations, recurrent",
+        "concept_id": 2576,
+        "category": "who_staging"
+    },
+    {
+        "name": "Unspecified stage 2 condition",
+        "concept_id": 7038,
+        "category": "who_staging"
+    },
+    {
+        "name": "Hepatosplenomegaly, persistent unexplained",
+        "concept_id": 7537,
+        "category": "who_staging"
+    },
+    {
+        "name": "Lineal gingival erythema",
+        "concept_id": 2891,
+        "category": "who_staging"
+    },
+    {
+        "name": "Wart virus infection, extensive",
+        "concept_id": 6775,
+        "category": "who_staging"
+    },
+    {
+        "name": "Molluscum contagiosum, extensive",
+        "concept_id": 6776,
+        "category": "who_staging"
+    },
+    {
+        "name": "Parotid enlargement, persistent unexplained",
+        "concept_id": 1210,
+        "category": "who_staging"
+    },
+    {
+        "name": "Asymptomatic HIV infection",
+        "concept_id": 5327,
+        "category": "who_staging"
+    },
+    {
+        "name": "Persistent generalized lymphadenopathy",
+        "concept_id": 5328,
+        "category": "who_staging"
+    }
 ]
