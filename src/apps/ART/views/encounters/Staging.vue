0ee555df8e9033142b25ee97ea102492ee5f33e8
Andrew Mfune <andiemfune@gmail.com>
2021-07-10 13:49:52 +0200

    Added cd4 result entry

--- src/apps/ART/views/encounters/Staging.vue
+++ src/apps/ART/views/encounters/Staging.vue
@@ -8,6 +8,9 @@ import { Field, Option } from "@/components/Forms/FieldInterface"
 import { toastWarning, toastSuccess } from "@/utils/Alerts"
 import { StagingService, StagingCategory } from "@/apps/ART/services/staging_service"
 import EncounterMixinVue from './EncounterMixin.vue'
+import Validation from "@/components/Forms/validations/StandardValidations"
+import MonthOptions from "@/components/FormElements/Presets/MonthOptions"
+import HisDate from "@/utils/Date"
 export default defineComponent({
     mixins: [EncounterMixinVue],
     data: () => ({
@@ -17,6 +20,12 @@ export default defineComponent({
         stageThreeObs: [] as any,
         stageTwoObs: [] as any,
         stageOneObs: [] as any,
+        cd4Available: false as boolean,
+        cd4Count: [] as any,
+        cd4Date: [] as any,
+        cd4Location: [] as any,
+        month: '' as string,
+        year: '' as string,
         summary: [] as any
     }),
     watch: {
@@ -43,9 +52,10 @@ export default defineComponent({
             const data = await Promise.all([
                 ...this.pregnancyObs, ... this.stageFourObs, 
                 ...this.stageThreeObs, ...this.stageTwoObs, 
-                ...this.stageOneObs
+                ...this.stageOneObs, ...this.cd4Count,
+                ...this.cd4Date, ...this.cd4Location
             ])
-            
+
             const obs = await this.staging.createObs(data)
 
             if (!obs) return toastWarning('Unable to save patient observations')
@@ -60,13 +70,20 @@ export default defineComponent({
                 { label: "No", value: "No" }
             ]
         },
-        async setPregnancyObsData(data: Array<Option>) {
+        async getFacilities(filter=''){
+            const facilities = await this.staging.getFacilities(filter)
+            return facilities.map((facility: any) => ({
+                label: facility.name,
+                value: facility.location_id
+            }))
+        },
+        setPregnancyObsData(data: Array<Option>) {
             const payload = data.map((obs: Option) => {
                 return this.staging.buildValueCoded(obs.other.concept, obs.value)
             })
-            this.pregnancyObs = await Promise.all(payload)
+            this.pregnancyObs = payload
         },
-        async buildStagingData(data: Array<Option>) {
+        buildStagingData(data: Array<Option>) {
             return data.map((item: Option) => {
                 return this.staging.buildValueCoded('Who stages criteria present', item.label)
             })
@@ -77,6 +94,9 @@ export default defineComponent({
                 value: concept.concept_id,
                 isChecked: false
             }))
+        },
+        hasCd4Count(f: any) {
+            return f.cd4_available && f.cd4_available.label === 'Yes'
         },
         getFields(): Array<Field> {
             return [
@@ -109,7 +129,7 @@ export default defineComponent({
                     id: 'stage_4_conditions',
                     helpText: 'Stage 4 conditions',
                     type: FieldType.TT_MULTIPLE_SELECT,
-                    unload: async (data: any) => this.stageFourObs = await this.buildStagingData(data),
+                    unload: (data: any) => this.stageFourObs = this.buildStagingData(data),
                     options: () => this.mapStagingOptions(
                         this.staging.isAdult() ? StagingCategory.ADULT_STAGE_4 : StagingCategory.PEDAID_STAGE_4 
                     )
@@ -118,7 +138,7 @@ export default defineComponent({
                     id: 'stage_3_conditions',
                     helpText: 'Stage 3 conditions',
                     type: FieldType.TT_MULTIPLE_SELECT,
-                    unload: async (data: any) => this.stageThreeObs = await this.buildStagingData(data),
+                    unload: async (data: any) => this.stageThreeObs = this.buildStagingData(data),
                     options: () => this.mapStagingOptions(
                         this.staging.isAdult() ? StagingCategory.ADULT_STAGE_3 : StagingCategory.PEDAID_STAGE_3 
                     )
@@ -127,7 +147,7 @@ export default defineComponent({
                     id: 'stage_2_conditions',
                     helpText: 'Stage 2 conditions',
                     type: FieldType.TT_MULTIPLE_SELECT,
-                    unload: async (data: any) => this.stageTwoObs = await this.buildStagingData(data),
+                    unload: async (data: any) => this.stageTwoObs = this.buildStagingData(data),
                     options: () => this.mapStagingOptions(
                         this.staging.isAdult() ? StagingCategory.ADULT_STAGE_2 : StagingCategory.PEDAID_STAGE_2 
                     )
@@ -136,10 +156,91 @@ export default defineComponent({
                     id: 'stage_1_conditions',
                     helpText: 'Stage 1 conditions',
                     type: FieldType.TT_MULTIPLE_SELECT,
-                    unload: async (data: any) => this.stageOneObs = await this.buildStagingData(data),
+                    unload: async (data: any) => this.stageOneObs = this.buildStagingData(data),
                     options: () => this.mapStagingOptions(
                         this.staging.isAdult() ? StagingCategory.ADULT_STAGE_1 : StagingCategory.PEDAID_STAGE_1 
                     )
+                },
+                {
+                    id: 'cd4_available',
+                    helpText: 'Recent CD4 count results available?',
+                    type: FieldType.TT_SELECT,
+                    unload: async (d: any) => this.cd4Available = d.value === 'Yes',
+                    validation: (val: any) => Validation.required(val),
+                    options: () => this.getYesNoOptions()
+                },
+                {
+                    id: 'cd4_count',
+                    helpText: 'CD4 Count',
+                    type: FieldType.TT_NUMBER,
+                    condition: (f: any) => this.hasCd4Count(f),
+                    unload: (d: Option) => {
+                        this.cd4Count = [this.staging.buildValueNumber('CD4 count', d.value)]
+                    },
+                    validation: (val: any) => Validation.required(val) || Validation.isNumber(val)
+                },
+                {
+                    id: 'year',
+                    helpText: 'Year of CD4 result',
+                    type: FieldType.TT_NUMBER,
+                    condition: (f: any) => this.hasCd4Count(f),
+                    unload: (d: Option) => this.year = `${d.value}`,
+                    validation(val: any) {
+                        const minYr = HisDate.getYearFromAge(100)
+                        const maxYr = HisDate.getCurrentYear()
+                        const notNum = Validation.isNumber(val)
+                        const noYear = Validation.required(val)
+                        const notInRange = Validation.rangeOf(val, minYr, maxYr)
+                        return noYear || notNum || notInRange
+                    }
+                },
+                {
+                    id: 'month',
+                    helpText: 'Month of CD4 result',
+                    type: FieldType.TT_SELECT,
+                    options: () => MonthOptions,
+                    unload: (d: Option) => this.month = `${d.value}`,
+                    condition: (f: any) => this.hasCd4Count(f),
+                    validation: (val: any) => {
+                        const date = `${this.year}-${this.month}-01`
+                        const notValid = HisDate.dateIsAfter(date) ? null : ['Month is greater than current month']
+                        const noMonth = Validation.required(val)
+                        return noMonth || notValid
+                    }
+                },
+                {
+                    id: 'day',
+                    helpText: 'Day of CD4 result',
+                    type: FieldType.TT_MONTHLY_DAYS,
+                    condition: (f: any) => this.hasCd4Count(f),
+                    unload: (d: Option) => {
+                        this.cd4Date = [this.staging.buildValueDate(
+                            'Cd4 count datetime', `${this.year}-${this.month}-${d.value}`
+                        )]
+                    },
+                    validation: (val: any) => {
+                        const day = val.value
+                        const date = `${this.year}-${this.month}-${day}`
+                        const notValid = HisDate.dateIsAfter(date) ? null : ['Date is greater than current date']
+                        const noDay = Validation.required(val)
+                        return noDay || notValid
+                    }
+                },
+                {
+                    id: 'location',
+                    helpText: 'CD4 Location',
+                    type: FieldType.TT_SELECT,
+                    group: 'person',
+                    unload: (d: Option) => {
+                        this.cd4Location = [this.staging.buildValueText('Cd4 count location', d.label)]
+                    },
+                    validation: (val: any) => Validation.required(val),
+                    condition: (f: any) => this.hasCd4Count(f),
+                    options: (_, filter='') => this.getFacilities(filter),
+                    config: {
+                        showKeyboard: true,
+                        isFilterDataViaApi: true
+                    }
                 },
                 {
                     id: '__form_summary__',
