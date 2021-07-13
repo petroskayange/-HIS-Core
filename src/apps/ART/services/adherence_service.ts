4f5c4f00ade8ac97c67fc2591d1c053562ded528
Andrew Mfune <andiemfune@gmail.com>
2021-07-13 09:20:02 +0200

    Added api for loading adherence drugs

--- src/apps/ART/services/adherence_service.ts
+++ src/apps/ART/services/adherence_service.ts
@@ -1,8 +1,28 @@
 import { AppEncounterService } from "@/services/app_encounter_service"
 import HisDate from "@/utils/Date"
+import { DrugInterface } from "@/interfaces/Drug"
+import { DrugOrderService } from "@/services/drug_order_service"
+
 export class AdherenceService extends AppEncounterService {
+    lastDrugs: Array<DrugInterface>
     constructor(patientID: number) {
         super(patientID, 68) //TODO: Use encounter type reference name
+        this.lastDrugs = []
+    }
+
+    async loadPreviousDrugs() {
+        const drugs = await DrugOrderService.getLastDrugsReceived(super.patientID)
+        let startDate = ''
+
+        if (!drugs) return
+
+        this.lastDrugs = drugs.filter((drug: DrugInterface) => {
+            if (!startDate) {
+                startDate = drug.start_date
+                return true
+            }
+            return new Date(drug.start_date) >= new Date(startDate)
+        })
     }
 
     calculateAdherence(given: number, pills: number, expected: number) {
