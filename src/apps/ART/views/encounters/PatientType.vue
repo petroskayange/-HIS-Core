624557aa8985ab8f3ddf199e7b42b17891772a6a
Martin Suleman <sulemartin87@gmail.com>
2021-07-19 18:52:46 +0200

    Worked on feedback from pull request

--- src/apps/ART/views/encounters/PatientType.vue
+++ src/apps/ART/views/encounters/PatientType.vue
@@ -13,9 +13,7 @@ import { defineComponent } from "vue";
 import { FieldType } from "@/components/Forms/BaseFormElements";
 import HisStandardForm from "@/components/Forms/HisStandardForm.vue";
 import Validation from "@/components/Forms/validations/StandardValidations";
-import { ObservationService } from "@/services/observation_service";
 import EncounterMixinVue from "./EncounterMixin.vue";
-import { ConceptService } from "@/services/concept_service";
 import { PatientTypeService } from "@/apps/ART/services/patient_type_service";
 import { toastSuccess, toastWarning } from "@/utils/Alerts";
 
@@ -57,10 +55,10 @@ export default defineComponent({
       }
     },
     async setPatientType() {
-      await ObservationService.getAll(this.patientID, "Type of patient").then(
+      await PatientTypeService.getAll(this.patientID, "Type of patient").then(
         async (data) => {
           if (data.length > 0) {
-            this.patientType = await ConceptService.getConceptName(
+            this.patientType = await PatientTypeService.getConceptName(
               data[0].value_coded
             );
           }
@@ -73,10 +71,6 @@ export default defineComponent({
           id: "patient_type",
           helpText: `Current type: ${this.patientType}`,
           type: FieldType.TT_SELECT,
-          config: {
-            showKeyboard: false,
-            showSummary: false,
-          },
           validation: (val: any) =>
             Validation.required(val) ||
             Validation.notTheSame(val, this.patientType),
