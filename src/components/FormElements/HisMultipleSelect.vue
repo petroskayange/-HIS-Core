efedd36c5a7b5ec08ad50a7702fede05560be320
Andrew Mfune <andiemfune@gmail.com>
2021-07-21 17:13:56 +0200

    (Refactor) Handle onValue callback if present.
    onselect now emits new values instead of the watcher

--- src/components/FormElements/HisMultipleSelect.vue
+++ src/components/FormElements/HisMultipleSelect.vue
@@ -1,25 +1,29 @@
 <template>
     <div>
       <view-port :showFull="!showKeyboard">
-      <his-text-input  v-if="showKeyboard" :value="selected" @onValue="(value) => onKbValue(value, showKeyboard)" :disabled="false"/>
-      <span v-for="(item, index) in checkedItems" :key="index"> 
-        <ion-chip color="danger" @click="uncheck(item.label)">{{item.label}}</ion-chip>
-      </span>
-      <ion-list class='view-port-content'>
-        <ion-item v-for="(entry, index) in filtered" :key="index" :color="entry.isChecked ? 'light':''">
-          <ion-label> 
-            <ion-text>
-              {{ entry.label }} 
-            </ion-text>
-            <ion-text
-              :color="entry.description.color" 
-              v-if="isDescription(entry.description, entry.isChecked)"> 
-              <p><i>{{ entry.description.text }}</i></p>
-            </ion-text>
-          </ion-label>
-          <ion-checkbox v-model="entry.isChecked" slot="end" :disabled="entry?.disabled"/>
-      </ion-item>
-      </ion-list>
+        <his-text-input  v-if="showKeyboard" :value="selected" @onValue="(value) => onKbValue(value, showKeyboard)" :disabled="false"/>
+        <span v-for="(item, index) in checkedItems" :key="index"> 
+          <ion-chip color="danger" @click="uncheck(item.label)">{{item.label}}</ion-chip>
+        </span>
+        <ion-list class='view-port-content'>
+          <ion-item v-for="(entry, index) in filtered" :key="index" :color="entry.isChecked ? 'light':''">
+            <ion-label> 
+              <ion-text>
+                {{ entry.label }} 
+              </ion-text>
+              <ion-text
+                :color="entry.description.color" 
+                v-if="isDescription(entry.description, entry.isChecked)"> 
+                <p><i>{{ entry.description.text }}</i></p>
+              </ion-text>
+            </ion-label>
+            <ion-checkbox
+              slot="end"
+              v-model="entry.isChecked"
+              @ionChange="(e) => onselect(entry, e)"
+              :disabled="entry?.disabled"/>
+         </ion-item>
+        </ion-list>
       </view-port>
       <his-keyboard v-if="showKeyboard" :kbConfig="keyboard" :onKeyPress="keypress"/>
     </div>
@@ -28,13 +32,28 @@
 import { Option, OptionDescriptionInterface } from "../Forms/FieldInterface";
 import { defineComponent } from "vue";
 import { IonCheckbox } from "@ionic/vue";
-import { find } from "lodash"
+import { find, isEmpty } from "lodash"
 import SelectMixin from "@/components/FormElements/SelectMixin.vue"
+
 export default defineComponent({
   components: { IonCheckbox },
   name: "HisMultipleSelect",
   mixins: [SelectMixin],
   methods: {
+    async onselect(entry: Option, event: any){
+      this.$nextTick(async ()=> {
+        const option = {...entry}
+        option.isChecked = event.target.checked
+
+        if (this.onValue && option.isChecked) {
+          const ok = await this.onValue(option)
+          if (!ok) {
+            return event.target.checked = false
+          } 
+        }
+        this.$emit('onValue', this.getChecked(this.listData))
+      })
+    },
     /*
       * Update existing list with new options while maintaining previously selected items
     */
@@ -48,9 +67,8 @@ export default defineComponent({
 
         if (itemChecked) return itemChecked
 
-        if (!('isChecked' in item)) {
-          item.isChecked = false
-        }
+        if (!('isChecked' in item)) item.isChecked = false
+
         return item
       })
     },
@@ -63,13 +81,12 @@ export default defineComponent({
       return list.filter((item: Option) => item.isChecked)
     },
     isDescription(description: OptionDescriptionInterface, isChecked=false) {
-      if (!description) return false
+      if (!description) 
+        return false
 
       if (description?.show) {
-          return (description.show === 'onChecked' 
-                  && isChecked || description.show === 'always')
+          return (description.show === 'onChecked' && isChecked || description.show === 'always')
       }
-
       return true
     },
   },
@@ -81,24 +98,12 @@ export default defineComponent({
   watch: {
     clear(isClear: boolean){
       if (isClear) {
-        // uncheck items
         this.listData = this.listData.map((item) => {
           item.isChecked = false
           return item
         })
         this.$emit('onClear')
       }
-    },
-    listData: {
-      handler(updatedItems: Array<Option>) {
-        // clear search string entered via keyboard
-        this.filter = ''
-  
-        const values = this.getChecked(updatedItems);
-
-        this.$emit("onValue", values);
-      },
-      deep: true
     }
   },
   async activated() {
