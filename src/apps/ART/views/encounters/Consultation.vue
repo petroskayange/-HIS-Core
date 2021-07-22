136e18912820aff228809f9dc89c4b461fbb17ad
Martin Suleman <sulemartin87@gmail.com>
2021-07-22 19:01:57 +0200

    starting validations

--- src/apps/ART/views/encounters/Consultation.vue
+++ src/apps/ART/views/encounters/Consultation.vue
@@ -13,22 +13,27 @@ import { FieldType } from "@/components/Forms/BaseFormElements";
 import HisStandardForm from "@/components/Forms/HisStandardForm.vue";
 import Validation from "@/components/Forms/validations/StandardValidations";
 import EncounterMixinVue from "./EncounterMixin.vue";
-import { PatientTypeService } from "@/apps/ART/services/patient_type_service";
 import { toastSuccess, toastWarning } from "@/utils/Alerts";
 import HisDate from "@/utils/Date";
 import MonthOptions from "@/components/FormElements/Presets/MonthOptions";
+import { ConsultationService } from "@/apps/ART/services/consultation_service";
+import { Patientservice } from "@/services/patient_service";
 
 export default defineComponent({
   mixins: [EncounterMixinVue],
   components: { HisStandardForm },
   data: () => ({
     fields: [] as any,
+    consultation: {} as any,
   }),
   watch: {
     $route: {
       async handler({ query }: any) {
         this.patientID = query.patient_id;
         this.fields = this.getFields();
+        this.consultation = new ConsultationService(this.patientID);
+        const response = await Patientservice.findByID(this.patientID);
+        this.patient = new Patientservice(response);
       },
       deep: true,
       immediate: true,
@@ -36,6 +41,7 @@ export default defineComponent({
   },
   methods: {
     async onFinish(formData: any) {
+      
       //       const pts = new PatientTypeService(this.patientID);
       //       const encounter = await pts.createEncounter();
       //       if (encounter) {
@@ -50,6 +56,35 @@ export default defineComponent({
       //       } else {
       //         return toastWarning("Unable to create treatment encounter");
       //       }
+    },
+    isGender(gender: string) {
+     return  this.patient.getGender() === gender;
+    },
+    isOfChildBearingAge() {
+      const age = this.patient.getAge();
+      return  age >= 9 && age <= 55;
+    },
+    ontubalLigation() {
+      return true;
+    },
+    showPregnancyQuestions() {
+      return  this.isGender('F') && this.isOfChildBearingAge() && this.ontubalLigation();
+    },
+    showCurrentContraceptionMethods(formData: any) {
+      return this.showPregnancyQuestions() && !this.isPregnant(formData);
+    },
+    showNewContraceptionMethods(formData: any) {
+      return this.showPregnancyQuestions() && !this.isPregnant(formData) && !this.isOnTubalLigation(formData);
+    },
+    isPregnant(formData: any) {
+      return formData.pregnant_breastfeeding.filter((data: any) => data.value === "Yes").length > 0;
+    },
+    isOnTubalLigation(formData: any) {
+      return formData.current_fp_methods.filter((data: any) => data.value === "TUBAL LIGATION").length > 0;
+    },
+    offerCxCa() {
+      //if is a female and cervical cancer is enabled
+      this.isGender('F')
     },
     getYesNo() {
       return [
@@ -78,11 +113,45 @@ export default defineComponent({
         return { label: method, value: method };
       });
     },
+    async getOptions(options: string[]) {
+        return options.map((data: any) => {
+          return {
+            label: data,
+            value: "",
+            other: {
+              values: this.getYesNo(),
+            },
+          };
+        });
+    },
+    getContraindications() {
+      const contraIndications = ["Peripheral neuropathy","Jaundice","Lipodystrophy","Kidney Failure","Psychosis","Gynaecomastia","Anemia","Skin rash","Insomnia", "Other"];
+      return this.getOptions(contraIndications);
+    },
+    getOtherContraindications() {
+      const contraIndications = ["Fever","Vomiting","Dizziness","Headache","Night Sweats","Nausea","Weight loss / Failure to thrive / malnutrition","Lactic acidosis","Cough", "Heavy alcohol use", "Other (Specify)"];
+      return this.getOptions(contraIndications);
+    },
     getFields(): any {
       return [
         {
-          id: "patient_type",
-          helpText: `Current type`,
+          id: "guardian_only_prescription",
+          helpText: "Medication to prescribe during this visit",
+          type: FieldType.TT_MULTIPLE_SELECT,
+          condition: () => false ,// show if guardian only visit
+          options: () => {
+            return [
+              { label: "ARVs", value: "ARVs" },
+              { label: "CPT", value: "CPT" },
+              { label: "IPT", value: "IPT" },
+              { label: "NONE OF THE ABOVE", value: "NONE OF THE ABOVE" },
+            ];
+          },
+        },
+        {
+          id: "pregnant_breastfeeding",
+          helpText: `Patient Pregnant or breastfeeding?`,
+          condition: () => this.showPregnancyQuestions(),
           type: FieldType.TT_MULTIPLE_YES_NO,
           options: () => [
             {
@@ -129,11 +198,13 @@ export default defineComponent({
         {
           id: "current_fp_methods",
           helpText: "What method are you currently on?",
+          condition: (formData: any) => this.showCurrentContraceptionMethods(formData),
           type: FieldType.TT_MULTIPLE_SELECT,
           options: () => this.getFPMethods(),
         },
         {
           id: "fp_methods",
+          condition: (formData: any) => this.showNewContraceptionMethods(formData),
           helpText: "What method are you providing today?",
           type: FieldType.TT_MULTIPLE_SELECT,
           options: () => this.getFPMethods(),
@@ -141,6 +212,7 @@ export default defineComponent({
         {
           id: "reason_for_no_fpm",
           helpText: "Main reason for not using family planning methods",
+          //show when no method is being provided today
           type: FieldType.TT_SELECT,
           options: () => {
             return [
@@ -154,8 +226,8 @@ export default defineComponent({
                 value: "Not needed for medical reasons",
               },
               {
-                label: "At risk of unplanned pregnanct",
-                value: "At risk of unplanned pregnanct",
+                label: "At risk of unplanned pregnancy",
+                value: "At risk of unplanned pregnancy",
               },
             ];
           },
@@ -163,6 +235,7 @@ export default defineComponent({
         {
           id: "specific_reason_for_no_fpm",
           helpText: "Specific reason for not using family planning methods",
+          //show when reason for no fpm is at risk of unplanned pregnancy
           type: FieldType.TT_SELECT,
           options: () => {
             return [
@@ -189,6 +262,7 @@ export default defineComponent({
         {
           id: "offer_contraceptive",
           helpText: "Offer contraceptives",
+          //show when previous one has a value
           type: FieldType.TT_SELECT,
           options: () => {
             return [
@@ -201,18 +275,21 @@ export default defineComponent({
         {
           id: "offered_intervention",
           helpText: "Offered intervention",
+          //show when the previous one is accepted
           type: FieldType.TT_SELECT,
           options: () => this.getFPMethods(),
         },
         {
           id: "offer_cxca",
           helpText: "Refer client for CxCa screening",
+          condition: () => this.offerCxCa(),
           type: FieldType.TT_SELECT,
           options: () => this.getYesNo(),
         },
         {
           id: "reason_for_no_cxca",
           helpText: "Reason for denying CxCa screening",
+          //if previous values is not
           type: FieldType.TT_SELECT,
           options: () => {
             return [
@@ -224,7 +301,8 @@ export default defineComponent({
         },
         {
           id: "birth_year",
-          helpText: "Year of birth",
+          helpText: "Previous CxCa test year",
+          //if previous values is not due for screening
           type: FieldType.TT_NUMBER,
           validation(val: any) {
             if (Validation.required(val)) return ["Year value is required"];
@@ -237,6 +315,21 @@ export default defineComponent({
             if (val.label.match(/Unknown/i)) return;
 
             return notNum || notInRange;
+          },
+        },
+        {
+          id: "cxca_estimate",
+          helpText: "Estimated time since last CxCa screening",
+          type: FieldType.TT_SELECT,
+          condition: (form: any) => form.birth_year.value.match(/Unknown/i),
+          options: () => {
+            return [
+              { label: "6 months", value: "6 months" },
+              { label: "12 months", value: "12 months" },
+              { label: "18 months", value: "18 months" },
+              { label: "24 months", value: "24 months" },
+              { label: "Over 2 years", value: "Over 2 years" },
+            ];
           },
         },
         {
@@ -257,6 +350,7 @@ export default defineComponent({
             return noMonth || notValid;
           },
         },
+
         {
           id: "birth_day",
           helpText: "Birth day",
@@ -282,6 +376,18 @@ export default defineComponent({
           helpText: "On TB Treatment?",
           type: FieldType.TT_SELECT,
           options: () => this.getYesNo(),
+        },
+        {
+          id: "side_effects",
+          helpText: "Contraindications / Side effects (select either 'Yes' or 'No')",
+          type: FieldType.TT_MULTIPLE_YES_NO,
+          options: () => this.getContraindications(),
+        },
+        {
+          id: "other side_effects",
+          helpText: "Other Contraindications / Side effects (select either 'Yes' or 'No')",
+          type: FieldType.TT_MULTIPLE_YES_NO,
+          options: () => this.getOtherContraindications(),
         },
         {
           id: "tb_status",
@@ -333,8 +439,8 @@ export default defineComponent({
           type: FieldType.TT_MULTIPLE_SELECT,
           options: () => {
             return [
-              { label: "ARVs", value: "Unknown" },
-              { label: "CPT", value: "Unknown" },
+              { label: "ARVs", value: "ARVs" },
+              { label: "CPT", value: "CPT" },
               { label: "3HP (RFP + INH)", value: "3HP (RFP + INH)" },
               { label: "IPT", value: "IPT" },
               { label: "NONE OF THE ABOVE", value: "NONE OF THE ABOVE" },
