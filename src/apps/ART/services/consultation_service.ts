9d2dac307c0c21f0d10dc9c9cbddae2dbd98fbd6
Martin Suleman <sulemartin87@gmail.com>
2021-07-22 19:02:10 +0200

    starting validations

---
+++ src/apps/ART/services/consultation_service.ts
@@ -0,0 +1,6 @@
+import { AppEncounterService } from "@/services/app_encounter_service";
+export class ConsultationService extends AppEncounterService {
+  constructor(patientID: number) {
+    super(patientID, 53);
+  }
+}
