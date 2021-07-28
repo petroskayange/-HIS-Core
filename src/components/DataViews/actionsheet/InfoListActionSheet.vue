cc94ba1ebf9a6047c08b5237bcbd688a72c4d2fc
Andrew Mfune <andiemfune@gmail.com>
2021-07-28 14:27:16 +0200

    (Refactor) Actionsheet are now indipendent with own components.
    Accessing them through a parent object was difficult and inflexible

---
+++ src/components/DataViews/actionsheet/InfoListActionSheet.vue
@@ -0,0 +1,31 @@
+<template>
+    <action-header :title="title" :subtitle="subtitle" :color="color"> </action-header>
+    <ion-content>
+        <ul> 
+            <li v-for="(item, index) in list" :key="index">
+                <span v-html="item"> </span>
+            </li>
+        </ul>
+    </ion-content>
+    <action-footer :buttons="actionButtons"> </action-footer>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ActionSheetMixin from "@/components/DataViews/actionsheet/ActionSheetMixin.vue"
+
+export default defineComponent({
+    mixins: [ActionSheetMixin],
+    props: {
+        list: {
+            type: Array,
+            required: true
+        }
+    }
+})
+</script>
+<style scoped>
+    ul li {
+        margin: 5%;
+        font-size: 1.4em;
+    }
+</style>
