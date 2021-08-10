5d4b471c201bb7a89e878b66c04adc3e8a7d401d
Andrew Mfune <andiemfune@gmail.com>
2021-08-10 14:08:26 +0200

    Added modal footer and action buttons

--- src/components/DataViews/ArtDispensationModal.vue
+++ src/components/DataViews/ArtDispensationModal.vue
@@ -1,44 +1,52 @@
 <template>
-    <table> 
-        <thead> 
-            <tr>
-                <th colspan="6"> Some drug </th>
-            </tr>
-            <tr> 
-                <th>Total tab(s) needed </th>
-                <th colspan="2"> Available stock </th>
-                <th colspan="2"> Dispensed </th>
-                <th> </th>
-            </tr>
-            <tr> 
-                <th> </th>
-                <th> Pack size </th>
-                <th> Packs </th>
-                <th> Total Tab(s) </th>
-                <th> Packs </th>
-                <th> </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr> 
-                <td :rowspan="amountNeededRowSpan"> {{tabsNeeded}} tab(s) </td>
-            </tr>
-            <tr v-for="(list, index) in items" :key="index">
-                <td 
-                    v-for="(data, dIndex) in list" 
-                    :key="dIndex"
-                    :class="dIndex === 3 ? 'input-field' : ''"> 
-                    {{ data }} 
-                </td>
-                <td> 
-                    <ul class='btn-list'> 
-                        <li> <ion-button class='dispense-btn' size="large"> + </ion-button> </li>
-                        <li> <ion-button class='dispense-btn' size="large"> - </ion-button> </li>
-                    </ul>
-                </td>
-            </tr>
-        </tbody>
-    </table>    
+    <ion-content>
+        <table> 
+            <thead> 
+                <tr>
+                    <th colspan="6"> Cotrimoxazole (960mg) </th>
+                </tr>
+                <tr> 
+                    <th>Total tab(s) needed </th>
+                    <th colspan="2"> Available stock </th>
+                    <th colspan="2"> Dispensed </th>
+                    <th> </th>
+                </tr>
+                <tr> 
+                    <th> </th>
+                    <th> Pack size </th>
+                    <th> Packs </th>
+                    <th> Total Tab(s) </th>
+                    <th> Packs </th>
+                    <th> </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr> 
+                    <td :rowspan="amountNeededRowSpan"> {{tabsNeeded}} tab(s) </td>
+                </tr>
+                <tr v-for="(list, index) in items" :key="index">
+                    <td 
+                        v-for="(data, dIndex) in list" 
+                        :key="dIndex"
+                        :class="dIndex === 3 ? 'input-field' : ''"> 
+                        {{ data }} 
+                    </td>
+                    <td> 
+                        <ul class='btn-list'> 
+                            <li> <ion-button class='dispense-btn' size="large"> + </ion-button> </li>
+                            <li> <ion-button class='dispense-btn' size="large"> - </ion-button> </li>
+                        </ul>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </ion-content>
+    <ion-footer>
+        <ion-toolbar> 
+        <ion-button color="danger" size="large" slot="end"> Close </ion-button>
+        <ion-button color="success" size="large" slot="end"> Dispense </ion-button>
+        </ion-toolbar>
+    </ion-footer>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
@@ -76,13 +84,16 @@ export default defineComponent({
 </script>
 
 <style scoped>
+    table {
+        width: 100%;
+    }
     table, td, th {
         border: solid 1px #ccc;
         text-align: center;
         vertical-align: middle;
     }
     td {
-        height: 150px;
+        height: 120px;
     }
     .btn-list {
         list-style: none;
