7161ea0308d0f6ba1bbeb0b6688dab59a5f90798
Andrew Mfune <andiemfune@gmail.com>
2021-08-11 08:50:33 +0200

    Added drugorder loaders in dispensation service

--- src/apps/ART/services/dispensation_service.ts
+++ src/apps/ART/services/dispensation_service.ts
@@ -1,7 +1,27 @@
 import { AppEncounterService } from "@/services/app_encounter_service";
+import { DrugOrderService } from "@/services/drug_order_service";
 
 export class DispensationService extends AppEncounterService {
+    drugHistory: Array<any>;
+    currentDrugOrder: Array<any>;
+    
     constructor(patientID: number) {
         super(patientID, 54)
+        this.drugHistory = []
+        this.currentDrugOrder = []
+    }
+
+    async loadDrugHistory() {
+        const res = await DrugOrderService.getDrugOrderHistory(this.patientID)
+        if (res) {
+            this.drugHistory = res
+        }
+    }
+
+    async loadCurrentDrugOrder() {
+        const res = await DrugOrderService.getDrugOrders(this.patientID)
+        if (res) {
+            this.currentDrugOrder = res
+        }
     }
 }
